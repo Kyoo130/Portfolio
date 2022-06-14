30e5fd601f67b2a1ddf1a7d10a4c849d56cfd5df
Kyoo130 <kml9003@kakao.com>
2022-06-15 01:17:01 +0900

    :heavy_plus_sign: fortawesome 추가

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        aperiam aspernatur at consequatur cum cumque dolore enim excepturi fugit
+        hic id, molestiae necessitatibus nesciunt optio sed sint, totam
+        voluptates voluptatum. Quisquam veritatis, voluptatum! Ab culpa, dolores
+        esse facilis, impedit laborum maiores non nulla porro possimus quibusdam
+        rerum sed tempora temporibus.
+      </p>
+      <ul>
+        <li>
+          <ul>
+            <li>HTML/CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+          </ul>
+        </li>
+        <li>
+          <ul>
+            <li>회사로고 / 업무 내용 / 기간</li>
+            <li>회사로고 / 업무 내용 / 기간</li>
+            <li>회사로고 / 업무 내용 / 기간</li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default About;
