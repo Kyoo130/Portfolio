e73bf367af869f50eb2520ad27a23fa782483b46
Kyoo130 <kml9003@kakao.com>
2022-06-14 19:37:54 +0900

    :sparkles: 프로필 이미지 및 Home 컴포넌트 추가

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section>
+      <h2 className="a11y-hidden">자기소개</h2>
+      <p>안녕하세요. 서비스 운영 경험이 있는 프론트엔드 개발자 이규민입니다.</p>
+      <img src="/img/profile_02.png" alt="profile"/>
+      <button>Contact Me</button>
+    </section>
+  );
+};
+
+export default Home;
