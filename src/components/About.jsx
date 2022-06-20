bf018150bd973610f13101c454f28243cbbcd8f3
Kyoo130 <kml9003@kakao.com>
2022-06-21 00:47:42 +0900

    :bug: 레이아웃 깨짐 스타일 버그 수정

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ import {
   faEnvelope,
   faGraduationCap,
   faCodeBranch,
-  faB,
+  faB
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -26,65 +26,40 @@ const Title = styled.h2`
   text-transform: uppercase;
 `;
 
-const AboutUl = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-
-  @media (min-width: 320px) and (max-width: 767px) {
-    flex-direction: column;
-    align-items: center;
-    row-gap: 2rem;
-  }
+const ListCont = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(1, auto);
+  justify-content: center;
+  row-gap: 2.5rem;
 
   @media (min-width: 768px) {
-    flex-direction: inherit;
-    justify-content: space-between;
-    row-gap: 2.5rem;
-
-    :after {
-      display: block;
-      content: "";
-      width: 33.33%;
-      height: 100%;
-    }
+    grid-template-columns: repeat(3, auto);
+    justify-content: space-around;
   }
 `;
 
-const AboutLi = styled.li`
-  width: 100%;
-
-  @media (min-width: 768px) {
-    width: 33.33%;
-    row-gap: 1rem;
-  }
-`;
-
-const AboutDiv = styled.div`
+const TestLi = styled.li`
   display: flex;
-  width: 100%;
-  max-width: 16.5rem;
-  margin: 0 auto;
-  padding-left: 0.5rem;
 
-  & svg {
+  & > svg {
     color: #2a92d5;
     width: 32px;
     height: 32px;
     margin-right: 1.5rem;
   }
 
-  & p {
+  & > p {
     line-height: 1.5rem;
     font-weight: 300;
-  }
 
-  & strong {
-    font-size: 1.2rem;
-    font-weight: 600;
-  }
+    & > strong {
+      font-size: 1.1rem;
+      font-weight: 600;
+    }
 
-  & a:hover {
-    color: #2a92d5;
+    & > a:hover {
+      color: #2a92d5;
+    }
   }
 `;
 
@@ -92,100 +67,84 @@ const About = () => {
   return (
     <AboutCont>
       <Title>About Me</Title>
-      <AboutUl>
-        <AboutLi>
-          <AboutDiv>
-            <FontAwesomeIcon icon={faUser} />
-            <p>
-              <strong>이름</strong>
-              <br />
-              이규민 (Lee KyooMin)
-            </p>
-          </AboutDiv>
-        </AboutLi>
-        <AboutLi>
-          <AboutDiv>
-            <FontAwesomeIcon icon={faCalendar} />
-            <p>
-              <strong>생년월일</strong>
-              <br />
-              1990.03.11
-            </p>
-          </AboutDiv>
-        </AboutLi>
-        <AboutLi>
-          <AboutDiv>
-            <FontAwesomeIcon icon={faLocationDot} />
-            <p>
-              <strong>주소지</strong>
-              <br />
-              서울특별시 영등포구
-            </p>
-          </AboutDiv>
-        </AboutLi>
-        <AboutLi>
-          <AboutDiv>
-            <FontAwesomeIcon icon={faPhone} />
-            <p>
-              <strong>연락처</strong>
-              <br />
-              010-9231-7919
-            </p>
-          </AboutDiv>
-        </AboutLi>
-        <AboutLi>
-          <AboutDiv>
-            <FontAwesomeIcon icon={faEnvelope} />
-            <p>
-              <strong>이메일</strong>
-              <br />
-              kml9003@kakao.com
-            </p>
-          </AboutDiv>
-        </AboutLi>
-        <AboutLi>
-          <AboutDiv>
-            <FontAwesomeIcon icon={faGraduationCap} />
-            <p>
-              <strong>최종학력</strong>
-              <br />
-              국제대학교 졸업
-            </p>
-          </AboutDiv>
-        </AboutLi>
-        <AboutLi>
-          <AboutDiv>
-            <FontAwesomeIcon icon={faCodeBranch} />
-            <p>
-              <strong>깃헙</strong>
-              <br />
-              <a
-                href="https://github.com/Kyoo130"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://github.com/Kyoo130
-              </a>
-            </p>
-          </AboutDiv>
-        </AboutLi>
-        <AboutLi>
-          <AboutDiv>
-            <FontAwesomeIcon icon={faB} />
-            <p>
-              <strong>블로그</strong>
-              <br />
-              <a
-                href="https://velog.io/@kml9003"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://velog.io/@kml9003
-              </a>
-            </p>
-          </AboutDiv>
-        </AboutLi>
-      </AboutUl>
+      <ListCont>
+        <TestLi>
+          <FontAwesomeIcon icon={faUser} />
+          <p>
+            <strong>이름</strong>
+            <br />
+            이규민 (Lee KyooMin)
+          </p>
+        </TestLi>
+        <TestLi>
+          <FontAwesomeIcon icon={faCalendar} />
+          <p>
+            <strong>생년월일</strong>
+            <br />
+            1990.03.11
+          </p>
+        </TestLi>
+        <TestLi>
+          <FontAwesomeIcon icon={faLocationDot} />
+          <p>
+            <strong>주소지</strong>
+            <br />
+            서울특별시 영등포구
+          </p>
+        </TestLi>
+        <TestLi>
+          <FontAwesomeIcon icon={faPhone} />
+          <p>
+            <strong>연락처</strong>
+            <br />
+            010-9231-7919
+          </p>
+        </TestLi>
+        <TestLi>
+          <FontAwesomeIcon icon={faEnvelope} />
+          <p>
+            <strong>이메일</strong>
+            <br />
+            kml9003@kakao.com
+          </p>
+        </TestLi>
+        <TestLi>
+          <FontAwesomeIcon icon={faGraduationCap} />
+          <p>
+            <strong>최종학력</strong>
+            <br />
+            국제대학교 졸업
+          </p>
+        </TestLi>
+        <TestLi>
+          <FontAwesomeIcon icon={faCodeBranch} />
+          <p>
+            <strong>깃헙</strong>
+            <br />
+            <a
+              href="https://github.com/Kyoo130"
+              target="_blank"
+              rel="noreferrer"
+            >
+              https://github.com/Kyoo130
+            </a>
+          </p>
+        </TestLi>
+        <TestLi>
+          <FontAwesomeIcon icon={faB} />
+          <p>
+            <strong>블로그</strong>
+            <br />
+            <a
+              href="https://velog.io/@kml9003"
+              target="_blank"
+              rel="noreferrer"
+            >
+              https://velog.io/@kml9003
+            </a>
+          </p>
+        </TestLi>
+      </ListCont>
     </AboutCont>
   );
 };
