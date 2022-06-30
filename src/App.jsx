6ce08521a42ceddc5d80d09b49c22b01a4deebbc
Kyoo130 <kml9003@kakao.com>
2022-07-01 00:36:20 +0900

    :adhesive_bandage: 스타일 및 불필요 코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./css/App.css";
-import {useState} from "react";
 import {
   About,
   Footer,
@@ -7,36 +6,20 @@ import {
   Home,
   Projects,
   Skills,
-  Career,
+  Career
 } from "./components";
-import Modal from "./components/Modal";
 
 function App() {
-  const [openModal, setOpenModal] = useState(false);
-  const openModalEvent = (e) => {
-    setOpenModal(e)
-  }
-
   return (
     <div className="App">
       <Header />
       <main className="main">
-        <Home
-          openModalEvent={openModalEvent}
-        />
+        <Home />
         <About />
         <Skills />
         <Projects />
         <Career />
       </main>
-      {
-        openModal
-        ? <Modal
-            openModal={openModal}
-            openModalEvent={openModalEvent}
-          />
-        : null
-      }
       <Footer />
     </div>
   );
